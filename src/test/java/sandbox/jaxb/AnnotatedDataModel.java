c0eb5cdc20d71d437197f9d99ed4142187fb305d
assout <assout@users.noreply.github.com>
2016-09-25 22:48:07 +0900

    WIP:  src/test/java/sandbox/jaxb/AnnotatedDataModel.java |  22 ++  src/test/java/sandbox/jaxb/JAXBTest.java           | 232 +++++++++++++++++----  src/test/resources/jaxb/schema3.xsd                |  14 ++  src/test/resources/jaxb/schema4.xsd                |  19 ++  src/test/resources/jaxb/schema5.xsd                |  22 ++  src/test/resources/jaxb/schema6.xsd                |  19 ++  6 files changed, 287 insertions(+), 41 deletions(-)

---
+++ src/test/java/sandbox/jaxb/AnnotatedDataModel.java
@@ -0,0 +1,22 @@
+package sandbox.jaxb;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class AnnotatedDataModel {
+	@XmlElement(nillable = false, required = true)
+	public String name;
+	@XmlElementWrapper(name = "items")
+	@XmlElement(required = false)
+	public List<String> item;
+
+	@Override
+	public String toString() {
+		return "AnnotatedDataModel [name=" + name + ", item=" + item + "]";
+	}
+
+}
--- src/test/java/sandbox/jaxb/JAXBTest.java
+++ src/test/java/sandbox/jaxb/JAXBTest.java
@@ -6,7 +6,12 @@ import java.util.Arrays;
 import javax.xml.XMLConstants;
 import javax.xml.bind.JAXB;
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.ValidationEvent;
+import javax.xml.bind.ValidationEventHandler;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 
@@ -14,49 +19,194 @@ import org.junit.Test;
 
 public class JAXBTest {
 
-	@Test
-	public void testMarshal() throws Exception {
-		DataModel m = new DataModel();
-		m.name = "taro";
-		m.item = Arrays.asList("mikan", "ringo");
-		JAXB.marshal(m, System.out);
+	public static class Marshal {
+		@Test
+		public void testSimple() throws Exception {
+			DataModel m = new DataModel();
+			m.name = "taro";
+			m.item = Arrays.asList("mikan", "ringo");
+			JAXB.marshal(m, System.out);
+		}
+
+		@Test
+		public void testMarhasller() throws Exception {
+			DataModel m = new DataModel();
+			m.name = "taro";
+			m.item = Arrays.asList("mikan", "ringo");
+
+			JAXBContext newInstance = JAXBContext.newInstance(DataModel.class);
+
+			Marshaller createMarshaller = newInstance.createMarshaller();
+
+			createMarshaller.marshal(m, System.out);
+		}
 	}
 
-	@Test
-	public void testUnmarshal() throws Exception {
-		StringBuilder input = new StringBuilder();
-		input.append("<?xml version='1.0' encoding='UTF-8'?>");
-		input.append("<dataModel>");
-		input.append("	<name>jonh</name>");
-		input.append("	<items>");
-		input.append("		<item>neko</item>");
-		input.append("		<item>inu</item>");
-		input.append("	</items>");
-		input.append("</dataModel>");
-		DataModel unmarshal = JAXB.unmarshal(new StringReader(input.toString()), DataModel.class);
-		System.out.println(unmarshal);
-	}
+	public static class Unmarshal {
 
-	@Test
-	public void testUnmarshalWithSchema() throws Exception {
-		StringBuilder input = new StringBuilder();
-		input.append("<?xml version='1.0' encoding='UTF-8'?>");
-		input.append("<dataModel>");
-		input.append("	<name>jonh</name>");
-		input.append("	<items>");
-		input.append("		<item>neko</item>");
-		input.append("		<item>inu</item>");
-		input.append("	</items>");
-		input.append("</dataModel>");
+		@Test
+		public void testJAXB() throws Exception {
+			StringBuilder input = new StringBuilder();
+			input.append("<?xml version='1.0' encoding='UTF-8'?>");
+			input.append("<dataModel>");
+			input.append("	<name>jonh</name>");
+			input.append("	<items>");
+			input.append("		<item>neko</item>");
+			input.append("		<item>inu</item>");
+			input.append("	</items>");
+			input.append("</dataModel>");
+			DataModel unmarshal = JAXB.unmarshal(new StringReader(input.toString()), DataModel.class);
+			System.out.println(unmarshal);
+		}
 
-		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-		Schema schema = schemaFactory.newSchema(getClass().getClassLoader().getResource("jaxb/schema1.xsd"));
-		JAXBContext context = JAXBContext.newInstance(DataModel.class);
-		Unmarshaller unmarshaller = context.createUnmarshaller();
-		unmarshaller.setSchema(schema);
+		@Test
+		public void testUnmarshallerWithSchema() throws Exception {
+			StringBuilder input = new StringBuilder();
+			input.append("<?xml version='1.0' encoding='UTF-8'?>");
+			input.append("<dataModel>");
+			input.append("	<name>jonh</name>");
+			input.append("	<items>");
+			input.append("		<item>neko</item>");
+			input.append("		<item>inu</item>");
+			input.append("	</items>");
+			input.append("</dataModel>");
 
-		DataModel unmarshal = (DataModel) unmarshaller.unmarshal(new StringReader(input.toString()));
+			SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+			Schema schema = schemaFactory.newSchema(getClass().getClassLoader().getResource("jaxb/schema1.xsd"));
+			JAXBContext context = JAXBContext.newInstance(DataModel.class);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			unmarshaller.setSchema(schema);
 
-		System.out.println(unmarshal);
+			DataModel unmarshal = (DataModel) unmarshaller.unmarshal(new StringReader(input.toString()));
+
+			System.out.println(unmarshal);
+		}
+
+		@Test
+		public void testUnmarshalIgnoreNamespace() throws Exception {
+			StringBuilder input = new StringBuilder();
+			input.append("<?xml version='1.0' encoding='UTF-8'?>");
+			input.append("<dataModel>");
+			input.append("	<name>jonh</name>");
+			input.append("	<items>");
+			input.append("		<item>neko</item>");
+			input.append("		<item>inu</item>");
+			input.append("	</items>");
+			input.append("</dataModel>");
+
+			JAXBContext context = JAXBContext.newInstance(DataModel.class);
+			XMLInputFactory factory = XMLInputFactory.newFactory();
+			factory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, false);
+			XMLStreamReader createXMLStreamReader = factory.createXMLStreamReader(new StringReader(input.toString()));
+
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+
+			SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+			Schema schema = schemaFactory.newSchema(getClass().getClassLoader().getResource("jaxb/schema1.xsd"));
+			unmarshaller.setSchema(schema);
+
+			Object unmarshal = unmarshaller.unmarshal(createXMLStreamReader);
+
+			// DataModel unmarshal = (DataModel) unmarshaller.unmarshal(new
+			// StringReader(input.toString()));
+
+			System.out.println(unmarshal);
+		}
+
+		@Test
+		public void testUnmarshalWithSchemaNoNamespace() throws Exception {
+			StringBuilder input = new StringBuilder();
+			input.append("<?xml version='1.0' encoding='UTF-8'?>");
+			input.append("<dataModel>");
+			input.append("	<name>jonh</name>");
+			input.append("	<items>");
+			input.append("		<item>neko</item>");
+			input.append("		<item>inu</item>");
+			input.append("	</items>");
+			input.append("</dataModel>");
+
+			SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+			Schema schema = schemaFactory.newSchema(getClass().getClassLoader().getResource("jaxb/schema2.xsd"));
+			JAXBContext context = JAXBContext.newInstance(DataModel.class);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			unmarshaller.setSchema(schema);
+
+			DataModel unmarshal = (DataModel) unmarshaller.unmarshal(new StringReader(input.toString()));
+
+			System.out.println(unmarshal);
+		}
+
+		@Test
+		public void testWithSchema() throws Exception {
+			StringBuilder input = new StringBuilder();
+			input.append("<?xml version='1.0' encoding='UTF-8'?>");
+			input.append("<dataModel>");
+			input.append("	<name>jonh</name>");
+			input.append("	<items>");
+			input.append("		<item>neko</item>");
+			input.append("		<item>inu</item>");
+			input.append("	</items>");
+			input.append("</dataModel>");
+
+			SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+			Schema schema = schemaFactory.newSchema(getClass().getClassLoader().getResource("jaxb/schema4.xsd"));
+			JAXBContext context = JAXBContext.newInstance(DataModel.class);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			unmarshaller.setSchema(schema);
+
+			DataModel unmarshal = (DataModel) unmarshaller.unmarshal(new StringReader(input.toString()));
+
+			System.out.println(unmarshal);
+		}
+
+		@Test
+		public void testWithSchemaCheckEventHandler() throws Exception {
+			StringBuilder input = new StringBuilder();
+			input.append("<?xml version='1.0' encoding='UTF-8'?>");
+			input.append("<dataModel>");
+			input.append("	<name></name>");
+			input.append("	<items>");
+			input.append("	</items>");
+			input.append("</dataModel>");
+
+			SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+			Schema schema = schemaFactory.newSchema(getClass().getClassLoader().getResource("jaxb/schema5.xsd"));
+			JAXBContext context = JAXBContext.newInstance(DataModel.class);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			unmarshaller.setSchema(schema);
+			unmarshaller.setEventHandler(new ValidationEventHandler() {
+				@Override
+				public boolean handleEvent(ValidationEvent paramValidationEvent) {
+					System.out.println(paramValidationEvent);
+					return false;
+				}
+			});
+
+			DataModel unmarshal = (DataModel) unmarshaller.unmarshal(new StringReader(input.toString()));
+
+			System.out.println(unmarshal);
+		}
+
+		@Test
+		public void testWithSchemaCheck() throws Exception {
+			StringBuilder input = new StringBuilder();
+			input.append("<?xml version='1.0' encoding='UTF-8'?>");
+			input.append("<dataModel>");
+			input.append("	<name>test</name>");
+//			input.append("	<items>");
+//			input.append("		<item>aa</item>");
+//			input.append("	</items>");
+			input.append("</dataModel>");
+
+			SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+			Schema schema = schemaFactory.newSchema(getClass().getClassLoader().getResource("jaxb/schema5.xsd"));
+			JAXBContext context = JAXBContext.newInstance(DataModel.class);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			unmarshaller.setSchema(schema);
+
+			DataModel unmarshal = (DataModel) unmarshaller.unmarshal(new StringReader(input.toString()));
+
+			System.out.println(unmarshal);
+		}
 	}
 }
