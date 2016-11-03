6c780e1da54ed7ad913d7791cf5926516a87ef40
assout <assout@users.noreply.github.com>
2016-11-03 13:30:28 +0900

    WIP:  maven-it/pom.xml                                | 108 ++++++++++++++++++++++++  maven-it/src/it/java/com/it/BarIT.java          |  11 +++  maven-it/src/test/java/com/sandbox/FooTest.java |  11 +++  3 files changed, 130 insertions(+)

---
+++ maven-it/src/it/java/com/it/BarIT.java
@@ -0,0 +1,11 @@
+package com.it;
+
+import org.junit.Test;
+
+public class BarIT {
+
+	@Test
+	public void test() {
+		System.out.println("it");
+	}
+}
---
+++ maven-it/src/test/java/com/sandbox/FooTest.java
@@ -0,0 +1,11 @@
+package com.sandbox;
+
+import org.junit.Test;
+
+public class FooTest {
+
+	@Test
+	public void test() {
+		System.out.println("ut");
+	}
+}
